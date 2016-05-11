5ab968bb7a5a9b297c3889b75c57392ef1fe5c2a
Yassin Ahmed <zenik_46@hotmail.com>
2016-05-11 12:26:09 +0200

    Corrigiendo los metodos de la clase Usuario, e interactuando con main

--- ChatApp/src/com/agora/chat/Main.java
+++ ChatApp/src/com/agora/chat/Main.java
@@ -4,7 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Inicializando chat");
+		
+		Grupo chat1 = new Grupo();
+		Usuario nuevo1 = new Usuario(chat1,"Yassin");
+		Usuario nuevo2 = new Usuario(chat1,"Gabriel");
+		Usuario nuevo3 = new Usuario(chat1, "Mehmet");
+		
+		
+		nuevo1.notify("Yassin", "Hola chicos");
+		nuevo2.notify("Gabriel", "Buenas que tal");
+		nuevo2.notify("Mehmet", "Hallo, was ist lost?");
+		
 	}
 
 }
--- ChatApp/src/com/agora/chat/Usuario.java
+++ ChatApp/src/com/agora/chat/Usuario.java
@@ -1,37 +1,41 @@
 package com.agora.chat;
 
-
 import com.agora.observerDP.*;
 
 public class Usuario implements Observer {
 
 	private String name;
-    
-	//Mantengo la referencia del grupo
+
+	// Mantengo la referencia del grupo
 	private Subjet grupo;
-	
 
-	public Usuario(Subjet grupo) {
+	public Usuario(Subjet grupo, String name) {
 
-		// Guardo la referencia  para el objeto Grupo asi
+		// Guardo la referencia para el objeto Grupo asi
 		// puedo hacer llamada de sus metodos
-
+		this.name = name;
 		this.grupo = grupo;
-
-		
 
 		// Mensaje que notifica de que hay un nuevo Usuario
 
 		System.out.println("New Observer " + this.name);
 
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+
+		return this.name;
 
 	}
 
 	@Override
 	public void notify(String name, String newMensaje) {
-		
-		System.out.println(name + " : " + newMensaje );
 
+		System.out.println(name + " : " + newMensaje);
 
 	}
 
